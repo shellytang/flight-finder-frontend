e2f117f35c2196ddc5e4c54a3e2dc83603eb041a
Shelly Tang <shellytang@gmail.com>
2017-10-23 08:42:54 -0700

    getting back list of airports

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,1 +1,35 @@
-'use strict';
+import React from 'react';
+import axios from 'axios';
+// import SearchBar from '../search-bar';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      airport: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`${__API_URL__}/api/airports`)
+      .then(res => {
+        let airport = res.data;
+        this.setState({airport});
+      });
+  }
+
+  componentDidUpdate() {
+    console.log(':::::STATE::::', this.state);
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>This is the App Component</h1>
+
+      </main>
+    );
+  }
+}
+
+export default App;
